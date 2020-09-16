b40d29a342b0f8ee3dfc90613dfae303213e236a
David Rynn <davidrynn@gmail.com>
2020-09-15 21:24:59 -0400

    Fix image sizing and preview   Set compression resistance on UIImageView wrapper to allow resizing   Added custom inits so preview can use a default image. TO DO: add internal gif for preview use.

--- Aminals/AnimalDetailView.swift
+++ Aminals/AnimalDetailView.swift
@@ -13,27 +13,47 @@ struct AnimalDetailView: View {
   let imageURL: URL
   let title: String
   let defaultImage = UIImage(systemName: "questionmark.square.fill")!
+  let isPreview: Bool
   @State private var requests = Set<AnyCancellable>()
   @State var image: UIImage = UIImage()
   @State var loading = true
   var body: some View {
     ZStack {
       ActivityView(animate: $loading, style: .large)
-      VStack {
-        Text(title)
-        AnimalImageView(image: $image)
-          .frame(minWidth: 0, maxWidth: 200, minHeight: 0, maxHeight: .infinity)
-          .padding(.horizontal, 50)
+      ZStack {
+        VStack {
+          Text(title)
+          AnimalImageView(image: self.$image)
+        }
       }
+      .padding(.horizontal, 50)
     }
     .onAppear {
-      self.fetchImageData(url: self.imageURL)
-        .sink { data in
-          self.image = UIImage.gifImageWithData(data) ?? self.defaultImage
-          self.loading = false
+      if self.isPreview {
+        self.image = self.defaultImage
+        self.loading = false
       }
-      .store(in: &self.requests)
+      else {
+        self.fetchImageData(url: self.imageURL)
+          .sink { data in
+            self.image = UIImage.gifImageWithData(data) ?? self.defaultImage
+            self.loading = false
+        }
+        .store(in: &self.requests)
+      }
     }
+  }
+
+  init(imageURL: URL, title: String) {
+    self.imageURL = imageURL
+    self.title = title
+    self.isPreview = false
+  }
+
+  init() {
+    self.imageURL = URL(string: "https:www.google.com")!
+    self.title = "Test Title For Preview"
+    self.isPreview = true
   }
 
   func fetchImageData(url: URL) -> AnyPublisher<Data, Never> {
@@ -46,8 +66,8 @@ struct AnimalDetailView: View {
   }
 }
 
-//struct AnimalDetailView_Previews: PreviewProvider {
-//    static var previews: some View {
-//      AnimalDetailView(imageURL: URL(string: "") ?? <#default value#>)
-//    }
-//}
+struct AnimalDetailView_Previews: PreviewProvider {
+  static var previews: some View {
+    AnimalDetailView()
+  }
+}
--- Aminals/AnimalImageView.swift
+++ Aminals/AnimalImageView.swift
@@ -14,14 +14,15 @@ struct AnimalImageView: UIViewRepresentable {
 
   func makeUIView(context: Context) -> UIImageView {
     let view = UIImageView(frame: .zero)
-//    view.contentScaleFactor = 0.5
+    view.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+    view.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
+
     view.contentMode = .scaleAspectFit
     return view
   }
 
   func updateUIView(_ uiView: UIImageView, context: Context) {
     uiView.image = image
-//    uiView.contentMode = .scaleAspectFit
-    uiView.contentScaleFactor = 0.2
+
   }
 }
